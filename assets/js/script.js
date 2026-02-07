cac774a45a7bbd1e08b529e60f5bdc2dad0424b6
Rethika <rambalam3@gatech.edu>
2026-02-07 17:45:43 -0500

    Nav bar links

--- assets/js/script.js
+++ assets/js/script.js
@@ -136,24 +136,46 @@ for (let i = 0; i < formInputs.length; i++) {
 
 
 
-// page navigation variables
+// // page navigation variables
 const navigationLinks = document.querySelectorAll("[data-nav-link]");
 const pages = document.querySelectorAll("[data-page]");
 
-// add event to all nav link
+// // add event to all nav link
+// for (let i = 0; i < navigationLinks.length; i++) {
+//   navigationLinks[i].addEventListener("click", function () {
+
+//     for (let j = 0; j < pages.length; j++) {
+//       if (this.innerHTML.toLowerCase() === pages[j].dataset.page) {
+//         pages[j].classList.add("active");
+//         navigationLinks[i].classList.add("active");
+//         window.scrollTo(0, 0);
+//       } else {
+//         pages[j].classList.remove("active");
+//         navigationLinks[j].classList.remove("active");
+//       }
+//     }
+
+//   });
+// }
+
 for (let i = 0; i < navigationLinks.length; i++) {
   navigationLinks[i].addEventListener("click", function () {
 
+    const targetPage = this.innerHTML.toLowerCase();
+
     for (let j = 0; j < pages.length; j++) {
-      if (this.innerHTML.toLowerCase() === pages[j].dataset.page) {
-        pages[j].classList.add("active");
-        navigationLinks[i].classList.add("active");
-        window.scrollTo(0, 0);
-      } else {
-        pages[j].classList.remove("active");
-        navigationLinks[j].classList.remove("active");
-      }
+      pages[j].classList.remove("active");
     }
 
+    for (let j = 0; j < navigationLinks.length; j++) {
+      navigationLinks[j].classList.remove("active");
+    }
+
+    document
+      .querySelector(`[data-page="${targetPage}"]`)
+      .classList.add("active");
+
+    this.classList.add("active");
+    window.scrollTo(0, 0);
   });
 }
